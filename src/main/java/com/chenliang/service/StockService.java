c1edf8396195e883f78a2c54b3966c3a0fac21e6
chenl <bac38595dc72>
2023-06-26 18:19:21 +0800

    redis分布式锁，用redistempalate实现

--- src/main/java/com/chenliang/service/StockService.java
+++ src/main/java/com/chenliang/service/StockService.java
@@ -7,12 +7,16 @@ import com.chenliang.pojo.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -31,19 +35,79 @@ public class StockService {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
-    public void deduct(){
-        //1.查询库存
-        String stock = this.redisTemplate.opsForValue().get("stock");
-        //2.判断库存是否充足
-        if(stock != null && stock.length() != 0){
-            Integer st = Integer.valueOf(stock);
-            if(st > 0){
-                //3.扣减库存
-                this.redisTemplate.opsForValue().set("stock",String.valueOf(--st));
+    //redis为共享资源，高并发会出现超买
+//    public void deduct(){
+//        //1.查询库存
+//        String stock = this.redisTemplate.opsForValue().get("stock");
+//        //2.判断库存是否充足
+//        if(stock != null && stock.length() != 0){
+//            Integer st = Integer.valueOf(stock);
+//            if(st > 0){
+//                //3.扣减库存
+//                this.redisTemplate.opsForValue().set("stock",String.valueOf(--st));
+//            }
+//        }
+//    }
+
+    /**
+     * REDIS 分布式锁 简单实现  --setnx key value 来实现
+     */
+    public void deduct (){
+        while (!this.redisTemplate.opsForValue().setIfAbsent("lock", "1111", 3, TimeUnit.SECONDS)){
+            //重试，循环
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
-
+        try{
+            //1.查询库存
+            String stock = this.redisTemplate.opsForValue().get("stock").toString();
+            //2.判断库存是否足够
+            if(stock != null && stock.length() != 0){
+                Integer st = Integer.valueOf(stock);
+                if(st>0){
+                    //3.扣减库存
+                    this.redisTemplate.opsForValue().set("stock",String.valueOf(--st));
+                }
+            }
+        }finally {
+            this.redisTemplate.delete("lock");
+        }
     }
+    /**
+     * REDIS 乐观锁 watch来实现 防止超买
+     */
+//    public void deduct(){
+//        redisTemplate.execute(new SessionCallback<Object>() {
+//            @Override
+//            public  Object execute(RedisOperations operations) throws DataAccessException {
+//                //watch
+//                operations.watch( "stock");
+//                //1.查询库存信息
+//                String stock = operations.opsForValue().get("stock").toString();
+//                //2.判断库存是否充足
+//                if (stock != null && stock.length() != 0) {
+//                    Integer st = Integer.valueOf(stock);
+//                    if (st > 0) {
+//                        //multi
+//                        operations.multi();
+//                        //3.扣减库存
+//                        redisTemplate.opsForValue().set("stock", String.valueOf(--st));
+//                        //exec
+//                        List<Object> exec = operations.exec();
+//                        if (exec == null || exec.size() == 0) {
+//                            deduct();
+//                        }
+//                        return exec;
+//                    }
+//                }
+//                return null;
+//            }
+//        });
+//    }
+
     //乐观锁，加version字段；update的时候判断version是否一样，如果不一样则减库存失败，再次调用方法，直到版本号没被改变。
 //    public void deduct(){
 //        //select ... for update 查询的时候就能加行锁了
